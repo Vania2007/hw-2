65546a5f2c1fda8a89a9384963ec1d033f88a122
Vania2007 <momotis2007@gmail.com>
2024-09-10 21:33:11 +0300

    add round()

--- 3,5.php
+++ 3,5.php
@@ -1,6 +1,6 @@
 <?php
 $radius = rand(1, 400);
 $diametr = $radius * 2;
-$s = M_PI * pow($radius, 2);
+$s = round(M_PI * pow($radius, 2));
 echo "<h1>Площа кола: $s px²
 <div style='width: {$diametr}px; height: {$diametr}px; background: green; border-radius: 50%;'></div>";
